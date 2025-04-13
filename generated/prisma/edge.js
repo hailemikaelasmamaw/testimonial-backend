67e4966fbd3e632f3a09a86b01ff22cd6f8026d4
Hailemikael Asmamaw <your-email@example.com>
2025-04-13 08:35:30 -0800

    prisma/client insatalled

--- generated/prisma/edge.js
+++ generated/prisma/edge.js
@@ -149,7 +149,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": "../../.env",
+    "rootEnvPath": null,
     "schemaEnvPath": "../../.env"
   },
   "relativePath": "../../prisma",
@@ -159,11 +159,12 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
+  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
         "fromEnvVar": "DATABASE_URL",
-        "value": "postgresql://postgres:=nmmBx8dX4n>zP$@localhost:5432/mydb"
+        "value": null
       }
     }
   },
