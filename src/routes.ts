49c2fae96162dc8b961c52851a95d664367eb13f
Hailemikael Asmamaw <your-email@example.com>
2025-04-14 15:04:58 -0800

    routes file created

---
+++ src/routes.ts
@@ -0,0 +1,8 @@
+import { Router } from 'express'
+import { TestimonialController } from './controllers/testimonialController';
+
+const routes = Router();
+
+const testimonialController = new TestimonialController()
+
+routes.post('/create', testimonialController.create)
